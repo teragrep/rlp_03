0b3d192b79e9f82db2c6b7f2b7e0e87a9812925a
Mikko Kortelainen <1@teragrep.com>
2022-09-14 15:11:43 +0300

    tests for 1.0-fixes branch

---
+++ src/test/java/com/teragrep/rlp_03/ManualTest.java
@@ -0,0 +1,26 @@
+package com.teragrep.rlp_03;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+
+public class ManualTest {
+    @Test // for testing with manual tools
+    @EnabledIfSystemProperty(named="runServerTest", matches="true")
+    public void runServerTest() throws IOException, InterruptedException {
+        final Consumer<byte[]> cbFunction;
+        AtomicLong asd = new AtomicLong();
+
+        cbFunction = (message) -> {
+            asd.getAndIncrement();
+        };
+        int port = 1601;
+        Server server = new Server(port, new SyslogFrameProcessor(cbFunction));
+        //server.setNumberOfThreads(4);
+        server.start();
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
