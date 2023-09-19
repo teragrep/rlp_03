6cfa10ae97258b43688c042bf8acf373a454b262
Strongest Number 9 <16169054+StrongestNumber9@users.noreply.github.com>
2023-09-19 14:41:39 +0300

    Disable multithread test on java 8 (#49)

--- src/test/java/com/teragrep/rlp_03/ServerShutdownTest.java
+++ src/test/java/com/teragrep/rlp_03/ServerShutdownTest.java
@@ -1,6 +1,8 @@
 package com.teragrep.rlp_03;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 import java.io.IOException;
 
@@ -15,6 +17,7 @@ public class ServerShutdownTest {
     }
 
     @Test
+    @DisabledOnJre(JRE.JAVA_8)
     public void testServerShutdownMultiThread() throws IOException, InterruptedException {
         Server server = new Server(10601, new SyslogFrameProcessor(System.out::println));
         server.setNumberOfThreads(8);
