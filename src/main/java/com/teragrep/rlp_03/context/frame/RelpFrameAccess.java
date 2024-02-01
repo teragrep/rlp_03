fe73f604e53e868fcaa8800c2302418d494008e9
Mikko Kortelainen <1@teragrep.com>
2024-02-01 11:50:54 +0200

    make RelpFrameAccess immutable

--- src/main/java/com/teragrep/rlp_03/context/frame/RelpFrameAccess.java
+++ src/main/java/com/teragrep/rlp_03/context/frame/RelpFrameAccess.java
@@ -6,52 +6,63 @@ import com.teragrep.rlp_03.context.frame.fragment.FragmentAccess;
 
 public class RelpFrameAccess implements RelpFrame {
 
-    private final RelpFrame relpFrame;
+    private final Fragment txn;
+    private final Fragment command;
+    private final Fragment payloadLength;
+    private final Fragment payload;
+    private final Fragment endOfTransfer;
+    private final boolean isStub;
+
     private final Access access;
 
     public RelpFrameAccess(RelpFrame relpFrame) {
-        this.relpFrame = relpFrame;
         this.access = new Access();
+        this.txn = new FragmentAccess(relpFrame.txn(), access);
+        this.command = new FragmentAccess(relpFrame.command(), access);
+        this.payloadLength = new FragmentAccess(relpFrame.payloadLength(), access);
+        this.payload = new FragmentAccess(relpFrame.payload(), access);
+        this.endOfTransfer = new FragmentAccess(relpFrame.endOfTransfer(), access);
+        this.isStub = relpFrame.isStub();
     }
     
     @Override
     public Fragment txn() {
-        return new FragmentAccess(relpFrame.txn(), access);
+        return txn;
     }
 
     @Override
     public Fragment command() {
-        return new FragmentAccess(relpFrame.command(), access);
+        return command;
     }
 
     @Override
     public Fragment payloadLength() {
-        return new FragmentAccess(relpFrame.payloadLength(), access);
+        return payloadLength;
     }
 
     @Override
     public Fragment payload() {
-        return new FragmentAccess(relpFrame.payload(), access);
+        return payload;
     }
 
     @Override
     public Fragment endOfTransfer() {
-        return new FragmentAccess(relpFrame.endOfTransfer(), access);
+        return endOfTransfer;
     }
 
     @Override
     public boolean isStub() {
-        return relpFrame.isStub();
+        return isStub;
     }
 
     @Override
     public String toString() {
-        return "RelpFrameImpl{" +
-                "txn=" + txn() +
-                ", command=" + command() +
-                ", payloadLength=" + payloadLength() +
-                ", payload=" + payload() +
-                ", endOfTransfer=" + endOfTransfer() +
+        return "RelpFrameAccess{" +
+                "txn=" + txn +
+                ", command=" + command +
+                ", payloadLength=" + payloadLength +
+                ", payload=" + payload +
+                ", endOfTransfer=" + endOfTransfer +
                 '}';
     }
 
