265e62b8e2fc477eea1ac453541e7f67d723bd46
Motoko Kusanagi <kordex@gmail.com>
2024-02-09 12:53:24 +0200

    comment out extra logging in RelpFrameTest

--- src/test/java/com/teragrep/rlp_03/context/frame/RelpFrameTest.java
+++ src/test/java/com/teragrep/rlp_03/context/frame/RelpFrameTest.java
@@ -44,7 +44,7 @@ public class RelpFrameTest {
         RelpFrameImpl relpFrame = new RelpFrameImpl();
     	for (int contentIter = 0; contentIter < contentBytes.length; contentIter++) {
     		// feed one at a time
-            LOGGER.info("contentIter <{}>", contentIter);
+            // LOGGER.info("contentIter <{}>", contentIter);
     		ByteBuffer input = ByteBuffer.allocateDirect(1);
     		input.put(contentBytes[contentIter]);
     		input.flip();
