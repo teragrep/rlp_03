7141ab7d69dfd8439ba4b07f1c7aa915fafcbfde
eemhu <125959687+eemhu@users.noreply.github.com>
2024-03-06 12:39:37 +0200

    revert Access/Lease/AccessTest to main branch. Move to another pull request.

--- src/main/java/com/teragrep/rlp_03/context/frame/access/Lease.java
+++ src/main/java/com/teragrep/rlp_03/context/frame/access/Lease.java
@@ -1,29 +1,74 @@
-package com.teragrep.rlp_03.context.frame.access;
+/*
+ * Java Reliable Event Logging Protocol Library Server Implementation RLP-03
+ * Copyright (C) 2021  Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
 
-import java.util.concurrent.Phaser;
+package com.teragrep.rlp_03.context.frame.access;
 
 public class Lease implements AutoCloseable {
 
     private final Access access;
-    private final Phaser subPhaser;
+
+    private volatile boolean isOpen;
     Lease(Access access) {
         this.access = access;
-        this.subPhaser = new Phaser(1);
+        this.isOpen = true;
     }
 
 
     @Override
     public void close() {
-        if (subPhaser.isTerminated()) {
-            throw new IllegalStateException("Lease phaser was terminated, can't close");
+        if (!isOpen) {
+            throw new IllegalStateException();
         }
         else {
-            subPhaser.arriveAndDeregister(); // should terminate
+            isOpen = false;
             access.release(this);
         }
     }
 
-    public boolean isTerminated() {
-        return subPhaser.isTerminated();
+    public boolean isOpen() {
+        return isOpen;
     }
 }
